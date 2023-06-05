01da9d704a5beb8517173ba319065764ce6dfa91
ADAMA NIADA <aniada1mg@gmail.com>
2023-06-05 04:09:46 +0100

    Message

--- database/migrations/20230605014303_development.js
+++ database/migrations/20230605014303_development.js
@@ -9,7 +9,8 @@ exports.up = function(knex) {
         .createTable('users', function (table) {
             table.increments('userId');
             table.string('phoneNumber').notNullable();
-            table.string('user_type', 40).notNullable();
+            table.string('user_type', 40).nullable();
+            table.string('role', 40).nullable();
             table.text('password').notNullable();
             table.timestamps();
         })
@@ -21,16 +22,55 @@ exports.up = function(knex) {
           
             table.foreign('author').references('userId').inTable('users');
         })
-        .createTable('user', function (table) {
+        .createTable('requettes', function (table) {
             table.increments('id');
-            table.string('first_name', 255).notNullable();
-            table.string('last_name', 255).notNullable();
+            table.integer('mobile_user_id').checkPositive();
+            table.integer('taxi_user_id').checkPositive();
+            table.string('lieu_depart', 150).notNullable();
+            table.string('lieu_arrive', 255).notNullable();
+            table.timestamp('heure_depart', { precision: 6 }).defaultTo(knex.fn.now(6));
+            table.float('distance').notNullable();
+            table.decimal('prix').notNullable();
+            table.timestamp('agree_or_desagree_at', { precision: 6 }).nullable();
+            table.boolean('status').nullable();
+            table.timestamp('paid_at', { precision: 6 }).nullable();
+            table.timestamps();
         })
-        .createTable('product', function (table) {
+        .createTable('locations', function (table) {
             table.increments('id');
-            table.decimal('price').notNullable();
-            table.string('name', 1000).notNullable();
-            table.text('bio');
+            table.integer('user_id').checkPositive();
+            table.string('latitude', 150).notNullable();
+            table.string('longitude', 150).notNullable();
+            table.string('altitude', 150).notNullable();
+            table.string('speed', 10).notNullable();
+            table.string('heart', 10).notNullable();
+            table.string('name', 10).notNullable();
+            table.text('description').nullable();
+            table.timestamps();
+        })
+        .createTable('notifications', function (table) {
+            table.increments('id');
+            table.integer('user_id').checkPositive();
+            table.text('description').notNullable();
+            table.boolean('status').nullable();
+            table.timestamps();
+        })
+        .createTable('favoris', function (table) {
+            table.increments('id');
+            table.integer('mobile_user_id').checkPositive();
+            table.integer('taxi_user_id').checkPositive();
+            table.text('commentaires').notNullable();
+            table.boolean('status');
+            table.timestamps();
+        })
+        .createTable('notes', function (table) {
+            table.increments('id');
+            table.integer('mobile_user_id').checkPositive();
+            table.integer('taxi_user_id').checkPositive().nullable();
+            table.string('note', 10).notNullable();
+            table.text('commentaires').notNullable();
+            table.boolean('status');
+            table.timestamps();
         });
 };
 
@@ -40,6 +80,11 @@ exports.up = function(knex) {
  */
 exports.down = function(knex) {
     return knex.schema
-        .dropTableIfExists("product")
-        .dropTableIfExists('user');
+        .dropTableIfExists("users")
+        .dropTableIfExists("posts")
+        .dropTableIfExists("requettes")
+        .dropTableIfExists("locations")
+        .dropTableIfExists("notifications")
+        .dropTableIfExists("favoris")
+        .dropTableIfExists('notes');
 };
