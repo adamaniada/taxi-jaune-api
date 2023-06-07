143345d2f898ec92b9a349c4783d645c7ba0f125
ADAMA NIADA <aniada1mg@gmail.com>
2023-06-07 16:51:18 +0100

    Update vercel env

---
+++ build_files.sh
@@ -0,0 +1,16 @@
+# # build the project
+# # echo "build the project"
+# pip install -r requirements.txt
+
+# # pillow installation
+# python -m pip install Pillow
+
+# # Make migration
+# # echo "Make migration"
+# python3.9 manage.py migrate --noinput
+# python3.9 manage.py makemigrations --noinput
+
+# # Collect static
+# # echo "Collect static"
+# # python3.9 manage.py collectstatic --noinput --clear
+# python3.9 manage.py collectstatic --noinput --clear
